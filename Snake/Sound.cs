305d42874168be4a560da1042ee3119989d15ac2
schul <schul@STEPPA_PIG>
2024-07-23 22:08:46 +0200

    Add project files.

---
+++ Snake/Sound.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Media;
+
+namespace Snake
+{
+    class Sound
+    {
+        public static void PlayMusic(string filepath)
+        {
+            SoundPlayer musicPlayer = new SoundPlayer();
+            musicPlayer.SoundLocation = filepath;
+            musicPlayer.Play();
+        }
+
+        static void PlayGameMusic()
+        {
+            if (Gameloop == 0
+        }
+    }
+}
